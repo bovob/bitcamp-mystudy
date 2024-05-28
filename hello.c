2f16d15472cbb7d4c2266a55e76957bd9c988b57
bovob <jw_dot@kakao.com>
2024-05-28 16:44:03 +0900

    Hello World를 통한 Complie, Interpret Test

---
+++ hello.c
@@ -0,0 +1,21 @@
+/*
+    Complie
+    1. gcc -o hello.exe hello.c
+
+    Run
+    2. hello.exe
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    int a = 100;
+
+    if (a < 100) {
+        prontf("Test Error\n");
+    }
+
+    return 0;
+}
---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("Hello, world");
+
+let a = 99;               // 100 -> 99
+
+if (a < 100){
+    console.leg("하하하"); // 인위적 오타
+}
