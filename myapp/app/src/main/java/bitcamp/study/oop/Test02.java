a4dd43df10da34dabd21a9f07470bd4b57cc993b
bovob <jw_dot@kakao.com>
2024-06-18 16:12:39 +0900

    myapp09

--- myapp/app/src/main/java/bitcamp/study/oop/Test02.java
+++ myapp/app/src/main/java/bitcamp/study/oop/Test02.java
@@ -1,18 +1,24 @@
 package bitcamp.study.oop;
 
+import bitcamp.study.oop.domain.Score;
+
 public class Test02 {
 
+    static Score[] scores = new Score[]{
+        new Score("홍길동", 100, 90, 85),
+        new Score("임꺽정", 90, 80, 75),
+        new Score("유관순", 80, 70, 65)
+    };
+
     public static void main(String[] args) {
-
-        Score s1 = new Score("홍길동", 10, 20, 30);
-        printScore(s1);
-
+        printScore();
     }
 
-    static void printScore(Score s1) {
-        System.out.printf("%s: %d, %d, %d, %d, %.1f\n", s1.name, s1.kor, s1.eng, s1.math, s1.sum,
-            s1.aver);
-
+    static void printScore() {
+        for (Score scores : scores) {
+            System.out.printf("%s: %d, %d, %d, %d, %.1f\n",
+                scores.getName(), scores.getKor(), scores.getEng(),
+                scores.getMath(), scores.getSum(), scores.getAver());
+        }
     }
-
 }
